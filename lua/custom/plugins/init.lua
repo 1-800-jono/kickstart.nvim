65d4e27f5679e463fcc3c760c7ec852db1ae78ba
Jonathan Perez <jonathan.phz@gmail.com>
2024-05-29 18:40:44 -0300

    Bufferline and some mappings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -19,51 +19,81 @@ return {
     },
     config = function()
       require('nvim-tree').setup {}
-      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
+      vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>', { silent = true })
     end,
   },
+  -- {
+  --   'akinsho/bufferline.nvim',
+  --   version = '*',
+  --   dependencies = 'nvim-tree/nvim-web-devicons',
+  --   config = function() -- This is the function that runs, AFTER loading
+  --     local bufferline = require 'bufferline'
+  --     bufferline.setup {
+  --       options = {
+  --         mode = 'buffers', -- set to "tabs" to only show tabpages instead
+  --         style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+  --         themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+  --         numbers = 'both',
+  --         close_command = 'bdelete! %d', -- can be a string | function, | false see "Mouse actions"
+  --         right_mouse_command = 'bdelete! %d', -- can be a string | function | false, see "Mouse actions"
+  --         left_mouse_command = 'buffer %d', -- can be a string | function, | false see "Mouse actions"
+  --         middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
+  --         indicator = {
+  --           icon = '▎', -- this should be omitted if indicator style is not 'icon'
+  --           style = 'icon', -- | 'underline' | 'none',
+  --         },
+  --         buffer_close_icon = '󰅖',
+  --         modified_icon = '●',
+  --         close_icon = '',
+  --         left_trunc_marker = '',
+  --         right_trunc_marker = '',
+  --         offsets = {
+  --           {
+  --             filetype = 'NvimTree',
+  --             text = 'File Explorer',
+  --             text_align = 'center',
+  --             separator = true,
+  --           },
+  --         },
+  --         color_icons = true, -- whether or not to add the filetype icon highlights
+  --         diagnostics = 'nvim_lsp',
+  --         diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  --           local icon = level:match 'error' and ' ' or ' '
+  --           return ' ' .. icon .. count
+  --         end,
+  --       },
+  --     }
+  --   end,
+  -- },
   {
-    'akinsho/bufferline.nvim',
-    version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function() -- This is the function that runs, AFTER loading
-      local bufferline = require 'bufferline'
-      bufferline.setup {
-        options = {
-          mode = 'buffers', -- set to "tabs" to only show tabpages instead
-          style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
-          themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
-          numbers = 'both',
-          close_command = 'bdelete! %d', -- can be a string | function, | false see "Mouse actions"
-          right_mouse_command = 'bdelete! %d', -- can be a string | function | false, see "Mouse actions"
-          left_mouse_command = 'buffer %d', -- can be a string | function, | false see "Mouse actions"
-          middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
-          indicator = {
-            icon = '▎', -- this should be omitted if indicator style is not 'icon'
-            style = 'icon', -- | 'underline' | 'none',
-          },
-          buffer_close_icon = '󰅖',
-          modified_icon = '●',
-          close_icon = '',
-          left_trunc_marker = '',
-          right_trunc_marker = '',
-          offsets = {
-            {
-              filetype = 'NvimTree',
-              text = 'File Explorer',
-              text_align = 'center',
-              separator = true,
-            },
-          },
-          color_icons = true, -- whether or not to add the filetype icon highlights
-          diagnostics = 'nvim_lsp',
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icon = level:match 'error' and ' ' or ' '
-            return ' ' .. icon .. count
-          end,
-        },
-      }
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
     end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- insert_at_start = true,
+      animation = false,
+      -- Set the filetypes which barbar will offset itself for
+      sidebar_filetypes = {
+        -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+        NvimTree = true,
+        -- Or, specify the text used for the offset:
+        undotree = {
+          text = 'undotree',
+          align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+        },
+        -- Or, specify the event which the sidebar executes when leaving:
+        -- ['neo-tree'] = { event = 'BufWipeout' },
+        -- Or, specify all three
+        -- Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+      },
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
   -- Git
   {
